14a29d7b975f415b2583508cf7cc0e2554305fd1
nkhumphreys <nkhumphreys@gmail.com>
2016-08-07 13:09:34 +0000

    Add edit and search views
    This commit contains an edit view for the document object and a search
    view using the full text search in the django.contrib.postgres module

--- documentregister/documents/urls.py
+++ documentregister/documents/urls.py
@@ -3,5 +3,7 @@ from documents import views
 
 urlpatterns = [
     url(r'^$', views.DocumentRegistrationView.as_view(), name='register'),
-    url(r'^document/(?P<pk>[^/]+)$', views.DocumentDetailView.as_view(), name='detail')
+    url(r'^document/(?P<pk>[^/]+)$', views.DocumentDetailView.as_view(), name='detail'),
+    url(r'^edit/(?P<pk>[^/]+)$', views.DocumentEditView.as_view(), name='edit'),
+    url(r'^search$', views.DocumentSearchView.as_view(), name='search')
 ]
--- documentregister/documents/views.py
+++ documentregister/documents/views.py
@@ -1,6 +1,8 @@
-from django.views.generic.edit import FormView, CreateView
+from django.views.generic.edit import CreateView, UpdateView
 from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
 from django.core.urlresolvers import reverse
+from django.contrib.postgres.search import SearchQuery, SearchRank, SearchVector
 
 from documents.models import Document
 
@@ -19,3 +21,36 @@ class DocumentRegistrationView(CreateView):
 class DocumentDetailView(DetailView):
     template_name = 'detail.html'
     model = Document
+
+
+class DocumentEditView(UpdateView):
+    template_name = 'register.html'
+    form_class = DocumentRegistrationForm
+    model = Document
+
+    def get_success_url(self):
+        return reverse('detail', kwargs={'pk': self.object.pk})
+
+
+class DocumentSearchView(ListView):
+    template_name = 'search.html'
+    model = Document
+    paginate_by = 25
+
+    def get_queryset(self):
+        q = self.request.GET.get('q')
+        if q:
+            vector = SearchVector('title', 'description', 'author_name', 'author_email')
+            query = SearchQuery(self.request.GET.get('q'))
+            object_list = Document.objects.annotate(
+                rank=SearchRank(vector, query),
+                search=vector
+            ).filter(search=query).order_by('-rank')
+        else:
+            object_list = Document.objects.all()
+        return object_list
+
+    def get_context_data(self, **kwargs):
+        ctx = super(DocumentSearchView, self).get_context_data(**kwargs)
+        ctx['query'] = self.request.GET.get('q', "")
+        return ctx
